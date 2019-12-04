cda4a48a0df99c7b439a796c5b7314e58b90c5bb
Shugo Maeda <shugo@ruby-lang.org>
2019-12-04 11:10:30 +0900

    Update

--- keyboards/keebio/viterbi/keymaps/custom/keymap.c
+++ keyboards/keebio/viterbi/keymaps/custom/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_ortho_5x14(
     KC_ESC,  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
     KC_PSCR, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    AG_TOGG, KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
+    KC_SLCK, KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
     KC_PGUP, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS, KC_RSFT ,
     KC_PGDN, ADJUST , KC_LGUI, KC_LALT, KC_LGUI, LOWER  , KC_SPC ,     KC_BSPC, RAISE  , KC_INS , KC_HOME, KC_END , KC_DEL , KC_RCTL
   ),
